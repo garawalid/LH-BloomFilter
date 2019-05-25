cdb4153a60b6c5ff91cb45ae55ab1323793fef5a
garawalid <gwalid94@gmail.com>
2019-05-26 00:15:27 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == "__main__":
+    setup(
+        name='LessHash-BloomFilter',
+        version='0.1',
+        packages=find_packages(exclude=['tests']),
+        license='Apache License v2',
+        maintainer='Walid Gara',
+        maintainer_email="",
+        description = 'Fast Bloom Filter',
+        url="https://github.com/garawalid/LH-BloomFilter"
+    )
