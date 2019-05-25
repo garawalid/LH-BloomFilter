21a304144ff956aa0aee1cf4a38a4859dbd8f5e5
garawalid <gwalid94@gmail.com>
2019-05-25 23:56:01 +0200

    Add docstring

--- lhbf/bloom.py
+++ lhbf/bloom.py
@@ -1,20 +1,34 @@
 from hash_function import hash_int, hash_str
 import numpy as np
+import warnings
 
 
 class BloomFilter():
 
-    def __init__(self, m, k):
+    def __init__(self, m=10000, k=3):
+        if m < 20:
+            raise warnings.warn("Please choose a higher m to reduce the probability of two random elements colliding")
         self.m = m  # length of victor
         self.k = k  # number of hash functions
-        self.vector = np.zeros(self.m, dtype=np.bool)
+        self.vector = np.zeros(self.m, dtype=np.bool)  # vector of bits
         self.n = 0  # number of elements added to the bloom filter
 
     def estimate_fpp(self):
         return (1 - np.exp(-self.k * self.n / self.m)) ** self.k
 
     def _compute_hash(self, x):
+        """
+        Compute k hashes for x
 
+        Parameters
+        ----------
+        x : str, int or None
+            element
+
+        Returns
+        -------
+        hash_values : list of hashes
+        """
         if x is None:
             x = 0
 
@@ -35,22 +49,51 @@ class BloomFilter():
         return hash_values
 
     def add(self, x):
+        """
+        Add element x to the bloom filter
+
+        Parameters
+        ----------
+        x : str, int or None
+            element
+        """
         hash_values = self._compute_hash(x)
         self.n += 1
         for idx in hash_values:
             self.vector[idx] = True
 
     def might_contain(self, x):
+        """
+        Check if x belongs to the bloom filter
+
+        Parameters
+        ----------
+        x : str, int or None
+            element
+
+        Returns
+        -------
+        False means that the element definitely doesn't exist.
+        True means that the element may exist
+        """
         hash_values = self._compute_hash(x)
 
         for idx in hash_values:
-            # if any result is equals to zero, then the element definitely doesn't exist.
+            # If any result is equals to zero, then the element definitely doesn't exist.
             if bool(self.vector[idx]) == False:
                 return False
         return True
 
     def combine(self, bloom_filter):
+        """
+        Combine two bloom filters
+
+        Parameters
+        ----------
+        bloom_filter : BloomFilter
+            Bloom filter to add
+        """
         if self.m == bloom_filter.m and self.k == bloom_filter.k:
             self.vector = np.logical_or(self.vector, bloom_filter.vector)
         else:
-            raise ValueError("Bloom filter should have the same parameters (m, k)")
+            raise ValueError("Bloom filters should have the same parameters (m, k)")
--- lhbf/hash_function.py
+++ lhbf/hash_function.py
@@ -1,10 +1,43 @@
 def hash_int(x, p):
+    """
+    Compute the hash of an integer. The hashing method is Knuth multiplicative hash.
+
+    Parameters
+    ----------
+    x : int
+        element
+    p : int
+        Prime number used for shifting. Recommended p is 53 or 31.
+
+    Returns
+    -------
+    hash : hash value of x
+    """
     if x < 0:
         x *= -1
-    return (x * 2654435769) >> (32 - p)
+    if 0 <= p <= 32:
+        return (x * 2654435769) >> (32 - p)
+    else:
+        raise ValueError("The prime number p should be between 0 and 32.")
 
 
 def hash_str(x, p, m):
+    """
+       Compute the hash of a string with polynomial rolling hash function.
+
+       Parameters
+       ----------
+       x : str
+           element
+       p : int
+           Prime number. Recommended p is 53.
+       m : int
+           The size of hashing space. Usually, The size of the bloom filter.
+
+       Returns
+       -------
+       hash : hash value of x
+       """
     hash_value = 0
     p_i = p
     for c in x:
