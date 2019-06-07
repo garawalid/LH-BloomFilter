0218b24d48fb165de55d883ab82954318ff96c3a
garawalid <gwalid94@gmail.com>
2019-06-07 21:35:15 +0200

    Fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 if __name__ == "__main__":
+
     setup(
         name='LessHash-BloomFilter',
-        version='0.0.2',
+        version='0.0.3',
         packages=find_packages(exclude=['tests']),
         license='Apache License v2',
         maintainer='Walid Gara',
         maintainer_email="",
         description = 'Fast Bloom Filter',
-        url="https://github.com/garawalid/LH-BloomFilter"
+        url="https://github.com/garawalid/LH-BloomFilter",
+        download_url ="https://github.com/garawalid/LH-BloomFilter/archive/v0.0.3.tar.gz",
+        long_description=long_description,
+        long_description_content_type='text/markdown'
     )
