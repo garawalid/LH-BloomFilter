0547146083e88e0b955a64eb3170c0cca26b5036
garawalid <gwalid94@gmail.com>
2019-06-07 21:46:43 +0200

    Fix pip install

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 from os import path
 
-
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
+try:
+    with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = None
 
 if __name__ == "__main__":
-
     setup(
         name='LessHash-BloomFilter',
-        version='0.0.3',
+        version='0.0.4',
         packages=find_packages(exclude=['tests']),
         license='Apache License v2',
         maintainer='Walid Gara',
         maintainer_email="",
-        description = 'Fast Bloom Filter',
+        description='Fast Bloom Filter',
         url="https://github.com/garawalid/LH-BloomFilter",
-        download_url ="https://github.com/garawalid/LH-BloomFilter/archive/v0.0.3.tar.gz",
+        download_url="https://github.com/garawalid/LH-BloomFilter/archive/v0.0.4.tar.gz",
         long_description=long_description,
         long_description_content_type='text/markdown'
     )
